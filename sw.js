be8b554b52d6afa1d5545c713a2bb718f4f3012f
Jonas Schilberg <jonasschilberg@MacBook-Air-2.fritz.box>
2026-05-13 23:31:11 +0200

    Fix Safari iOS double tap hover bug on cards

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // OpernLog Service Worker – Offline Caching
-const CACHE_NAME = 'opernlog-v22';
+const CACHE_NAME = 'opernlog-v23';
 
 // App shell files to cache for offline use
 const APP_SHELL = [
