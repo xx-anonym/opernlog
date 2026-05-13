0caa5d275aa1ddf5d5c5e7eaef7fb8deb317cfb6
Jonas Schilberg <jonasschilberg@MacBook-Air-2.fritz.box>
2026-05-13 23:35:26 +0200

    Improve back link visibility over background images

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // OpernLog Service Worker – Offline Caching
-const CACHE_NAME = 'opernlog-v23';
+const CACHE_NAME = 'opernlog-v24';
 
 // App shell files to cache for offline use
 const APP_SHELL = [
